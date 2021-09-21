c8a4f9ea34b3e9cd2950ade1556b9320d4eec6b9
Kun Lin <kun.lin.596@gmail.com>
2021-09-21 23:39:31 +0900

    Create common.h

--- src/planner.h
+++ src/planner.h
@@ -4,64 +4,9 @@
 #include "Eigen-3.3/Eigen/Core"
 #include "Eigen-3.3/Eigen/Dense"
 #include "Eigen-3.3/Eigen/QR"
-#include <iostream>
-#include <vector>
-#include <boost/format.hpp>
+#include "common.h"
 
 namespace pathplanning {
-
-///
-/// Data Structures
-///
-
-struct CarState {
-  std::array<double, 3> euclideanPose; ///< x, y, theta
-  std::array<double, 2> frenetPose; ///< s, d
-  double speed; ///< car speed, in MPH
-
-  CarState(
-    const std::array<double, 3> &euclideanPose,
-    const std::array<double, 2> &frenetPose,
-    double speed
-  )
-    : euclideanPose(euclideanPose)
-    , frenetPose(frenetPose)
-    , speed(speed)
-  {}
-};
-
-struct NaviMap {
-  // Euclidean position
-  std::vector<double> x;
-  std::vector<double> y;
-  // Frenet s value
-  std::vector<double> s;
-  // Normal vector w.r.t. x, y
-  std::vector<double> dx;
-  std::vector<double> dy;
-};
-
-using Path = std::vector<std::array<double, 2>>;
-
-inline std::ostream& operator<< (std::ostream &out, const std::array<double, 2> &array) {
-  out << (boost::format("[%.3f, %.3f]") % array[0] % array[1]).str();
-  return out;
-}
-
-inline std::ostream& operator<< (std::ostream &out, const std::array<double, 3> &array) {
-  out << (boost::format("[%.3f, %.3f, %.3f]") % array[0] % array[1] % array[2]).str();
-  return out;
-}
-
-inline std::ostream& operator<< (std::ostream &out, const pathplanning::CarState &state) {
-  out << "CarState=(euclideanPose="
-      << state.euclideanPose
-      << ", frenetPose="
-      << state.frenetPose
-      << ", speed=" << state.speed << ")";
-  return out;
-}
-
 ///
 /// Helper Functions
 ///
