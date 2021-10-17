0d061d3c66dd313212c8c1a650befdcbb9ed0ca6
Kun <kun.lin.596@gmail.com>
2021-10-17 17:09:03 +0900

    Refactor behavior planner

--- src/behaviorplanner.h
+++ src/behaviorplanner.h
@@ -1,32 +1,56 @@
 #ifndef BEHAVIORPLANNER_H
 #define BEHAVIORPLANNER_H
 
-#include "helpers.h"
+#include "map.h"
 
 namespace pathplanning {
 
-class BehaviorPlanner {
-public:
-
-  BehaviorPlanner(const NaviMap &map)
-    : _map(map) {}
-
-  /**
-   * Get successor states from given state
-   */
-  static std::vector<BehaviorState>
-    GetSuccessorStates(const BehaviorState &state, int laneId);
-
-  void Reset() {};
-
-  void SetBehavior(const Behavior &behavior) { _behavior = behavior; }
-  const Behavior& GetBehavior() const { return _behavior; }
-
-private:
-  Behavior _behavior;
-  const NaviMap &_map;
+/**
+ * @brief      Output structure of Behacior planner
+ */
+struct Target {
+  int targetLaneId;
+  int targetLeadingVehicleId;
+  double targetSpeed;
+  double secondsToReachTarget;
 };
 
-} // end of pathplanning
+/**
+ * @brief      States of FSM for behavior planner
+ *
+ * kLaneKeeping:
+ *  - d, stay near center line for lane
+ *  - s, drive at target speed when feasible
+ *
+ * kLeftLaneChange/kRightLaneChange:
+ *  - d, move left or right
+ *  - s, same rules as keep lane (for initial lane)
+ *
+ * kLeftLaneChangePreparation/kRightLaneChangePreparation:
+ *  - d, stay near cener line for current lane
+ *  - s, attempt to match position and speed of "gap" in lane
+ *  - signal, activate turning signal
+ *
+ */
+enum class BehaviorState : uint8_t {
+  kLaneKeeping = 0,
+  kLeftLaneChangePreparation,
+  kLeftLaneChange,
+  kRightLaneChangePreparation,
+  kRightLaneChange
+};
+
+/**
+ * @brief      This class describes a behavior planner.
+ */
+class BehaviorPlanner {
+ public:
+  BehaviorPlanner(const Map &map) : _map(map) {}
+
+ private:
+  const Map &_map;
+};
+
+}  // namespace pathplanning
 
 #endif
