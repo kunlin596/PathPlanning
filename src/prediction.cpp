628a2b4495c9493a336ecc7465242dabd80b75ca
Kun <kun.lin.596@gmail.com>
2021-10-17 16:47:02 +0900

    Create prediction module

---
+++ src/prediction.cpp
@@ -0,0 +1,3 @@
+#include "prediction.h"
+
+namespace pathplanning {}  // namespace pathplanning
---
+++ src/prediction.h
@@ -0,0 +1,6 @@
+#ifndef PATHPLANNING_PREDICTION_H
+#define PATHPLANNING_PREDICTION_H
+
+namespace pathplanning {}  // namespace pathplanning
+
+#endif
