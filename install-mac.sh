7eaeacd6853a38952419ff2324c51ed1059f2661
Kun <kun.lin.596@gmail.com>
2021-10-19 23:32:50 +0900

    Add log

--- install-mac.sh
+++ install-mac.sh
@@ -1,11 +1,25 @@
 #! /bin/bash
-brew install openssl libuv cmake zlib
+
+echo
+echo ----------------------
+echo Installing uWebSockets
+echo ----------------------
+echo
+
+brew install cmake openssl libuv zlib
+
+echo
+echo ----------------------
+echo Installing uWebSockets
+echo ----------------------
+echo
+
 git clone https://github.com/uWebSockets/uWebSockets 
 cd uWebSockets
 git checkout e94b6e1
 patch CMakeLists.txt < ../cmakepatch.txt
 mkdir build
-export PKG_CONFIG_PATH=/usr/local/opt/openssl/lib/pkgconfig 
+export PKG_CONFIG_PATH=/usr/local/opt/openssl/lib/pkgconfig
 cd build
 cmake ..
 make 
