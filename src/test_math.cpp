4181fe711d5223000155176a5d9ac5b47eb8229b
Kun Lin <kun.lin.596@gmail.com>
2021-10-27 22:40:28 +0900

    Add test_math

---
+++ src/test_math.cpp
@@ -0,0 +1,12 @@
+#include "log.h"
+#include "math.h"
+
+int main(int argc, char** argv) {
+  using namespace pathplanning;
+
+  QuinticFunctor func;
+  func = QuinticFunctor({0, 0, 0, 0, 0, 0});
+  assert(func(0.0) == 0.0);
+  func = QuinticFunctor({10.0, 0, 0, 0, 0, 0});
+  assert(func(0.0) == 10.0);
+}
