e2996b8d1b5bf7a9a9b42d9aa20cdaf84b2cdf73
Kun <kun.lin.596@gmail.com>
2021-11-02 18:54:19 +0900

    Clean up plot_trajectory

--- python/plot_trajectory.py
+++ python/plot_trajectory.py
@@ -2,51 +2,74 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import json
+import argparse
 
-if __name__ == '__main__':
-  with open('../build/traj1d_time_exp.json', 'r') as f:
-    j = json.load(f)
+NAMES = ["position", "velocity", "accelaration", "jerk", "snap", "crackle"]
+np.set_printoptions(suppress=True, precision=6)
 
-    all_polys = []
-    all_times = []
-    accu_time = 0.0
-    names = ['position', 'velocity', 'accelaration', 'jerk', 'snap', 'crackle']
-    for index, (k, w) in enumerate(j.items()):
-      # plt.figure(index + 1)
-      # plt.suptitle(f'{index + 1}')
-      plt.ion()
-      plt.show()
-      polys = []
-      for i in range(6):
-        polys.append(np.polynomial.Polynomial(w[f'func{i}']))
-      all_polys.append(polys)
 
-      t = w['time']
-      all_times.append(t)
+def evaluate_polynomial(coeffs, time, time_step=0.01):
+    poly = np.polynomial.Polynomial(coeffs)
+    curr_time = 0.0
+    times = []
+    values = []
+    while curr_time < time + 1e-6:
+        values.append(poly(curr_time))
+        times.append(curr_time)
+        curr_time += time_step
+    return times, values
 
-      curr_time = 0.0;
-      values = []
-      while curr_time < (t + 1e-6):
-        values.append([
-          poly(curr_time) for poly in polys
-        ])
-        curr_time += 0.02
-      values = np.array(values)
 
-      for i in range(6):
-        plt.subplot(3, 2, i + 1)
-        plt.title(names[i])
-        plt.plot(
-          np.arange(
-            accu_time,
-            accu_time + t + 1e-6,
-            0.02),
-          values[:, 5 - i],
-          linewidth=0.5)
+def plot1d(data):
+    plt.figure()
+    plt.ioff()
 
-      # plt.legend()
-      # accu_time += t
+    for i in range(6):
+        plt.subplot(6, 1, i + 1)
+        plt.title(NAMES[i])
+        times, values = evaluate_polynomial(data[f"func{5 - i}"], data["time"])
+        plt.plot(times, values)
 
+    plt.ion()
+    plt.show(block=False)
+
+
+def plot2d(data):
+    plt.figure(0)
+    plt.ion()
+
+    points = []
+    for dim, traj_data in enumerate(data):
+        times, values = evaluate_polynomial(traj_data["func5"], traj_data["time"])
+        points.append(values)
+
+    points = np.array(points).T
+
+    plt.plot(points[:, 0], points[:, 1], "r.", markersize=1)
+    plt.show(block=False)
+
+    # plot1d(data[0])
+    # plot1d(data[1])
 
     from IPython import embed
+
     embed()
+
+
+def main(filename):
+    with open(filename, "r") as f:
+        data = json.load(f)
+
+    if isinstance(data, list):
+        plot2d(data)
+    else:
+        plot1d(data)
+
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser(description="Visualize Trajectory File")
+    parser.add_argument("--filename", "-f", type=str, help="JMT trajectory file names")
+    args = parser.parse_args()
+
+    main(args.filename)
